bc8840085486b6a8e8cf2a800dd1338b0dfd3a1d
Josef Goodyear <jgoodyear@Josef-Goodyear-MacBook-Pro.local>
2020-10-21 14:55:28 -0700

    update spacing?

--- delete_all_sessions/delete_all_sessions.py
+++ delete_all_sessions/delete_all_sessions.py
@@ -3,13 +3,13 @@ import argparse
 from pdpyras import APISession
 
 def delete_user_sessions():
-	user_ids = []
-	user_sessions = []
-	for user in session.iter_all('users'):
-    	 user_ids.append(user['id'])
-	for user_id in user_ids:
-		print("deleting all sessions for user {} . . .".format(user_id))
-		session.rdelete('/users/{}/sessions'.format(user_id))
+    user_ids = []
+    user_sessions = []
+    for user in session.iter_all('users'):
+             user_ids.append(user['id'])
+    for user_id in user_ids:
+        print("deleting all sessions for user {} . . .".format(user_id))
+        session.rdelete('/users/{}/sessions'.format(user_id))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Deletes all user sessions for all users on an account")

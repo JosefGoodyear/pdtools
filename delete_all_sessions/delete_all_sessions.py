625eba6f64b92162bde6ad8d66d819af05418870
Josef Goodyear <jgoodyear@Josef-Goodyear-MacBook-Pro.local>
2020-11-09 15:53:14 -0800

    add -a and -k flags

--- delete_all_sessions/delete_all_sessions.py
+++ delete_all_sessions/delete_all_sessions.py
@@ -12,7 +12,6 @@ def get_users_from_file(f):
             user_emails.append(row[0])
     return user_emails 
 
-
 def add_users_from_file(user_emails):
     users = session.dict_all('users', by='email')
     user_ids = []
@@ -23,30 +22,33 @@ def add_users_from_file(user_emails):
             print("No user was found with login email: {}. Their sessions will not be deleted.".format(user_email))
     return user_ids
 
-
 def add_all_users():
     user_ids = []
     for user in session.iter_all('users'):    
         user_ids.append(user['id'])
     return user_ids
 
-
 def delete_user_sessions(user_ids):
     user_sessions = []
     for user_id in user_ids:
-        print("deleting all sessions for user {} . . .".format(user_id))
+        print("deleting sessions for user {} . . .".format(user_id))
         session.rdelete('/users/{}/sessions'.format(user_id))
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Deletes all user sessions for all users on an account")
-    parser.add_argument('-a', '--api-key', required=True, help="REST API key")
+    parser.add_argument('-k', '--api-key', required=True, help="REST API key")
     parser.add_argument('-f', '--file', required=False, help="File containing user login emails")
+    parser.add_argument('-a', '--all', action='store_true', help="delete sessions for all users")
     args = parser.parse_args()
     session = APISession(args.api_key)
     if args.file:
+        print("False")
         user_emails = get_users_from_file(args.file)
         user_ids = add_users_from_file(user_emails)
-    else:
+    elif args.all is True:
+        print("True")
         user_ids = add_all_users()
-    delete_user_sessions(user_ids)
+    if args.all is True or args.file:
+        delete_user_sessions(user_ids)
+    else:
+        print("Please use the -f flag to use a csv file or the -a flag to delete sessions for all users")
